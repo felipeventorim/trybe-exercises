a8edd8c9ee0c13ac804766187acda7089e812fac
Felipe Ventorim <felipeventorim11@gmail.com>
2021-05-19 18:32:05 -0300

    Exercicio 4 - positivo, negativo ou zero

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/JavaScript_-_Primeiros_passos.1/script2.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/JavaScript_-_Primeiros_passos.1/script2.js
@@ -1,10 +1,12 @@
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
 let num1 = 10;
 let num2 = 8;
 
 if (num1 > num2) {
-  console.log("O maior número é: " + num1);
+  console.log('O maior número é: ' + num1);
 } else if (num1 < num2) {
-  console.log("O maior número é: " + num2);
+  console.log('O maior número é: ' + num2);
 } else {
-  console.log("Os números são iguais");
+  console.log('Os números são iguais');
 }
--- bloco-4-introducao-a-javascript-e-logica-de-programacao/JavaScript_-_Primeiros_passos.1/script3.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/JavaScript_-_Primeiros_passos.1/script3.js
@@ -1,13 +1,15 @@
+// Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
+
 let num1 = 10;
 let num2 = 8;
 let num3 = 12;
 
 if (num1 > num2 && num1 > num3) {
-  console.log("O maior número é: " + num1);
+  console.log('O maior número é: ' + num1);
 } else if (num2 > num1 && num2 > num3) {
-  console.log("O maior número é: " + num2);
+  console.log('O maior número é: ' + num2);
 } else if (num3 > num1 && num3 > num2) {
-  console.log("O maior número é: " + num3);
+  console.log('O maior número é: ' + num3);
 } else {
-  console.log("Os números são iguais");
+  console.log('Os números são iguais');
 }
