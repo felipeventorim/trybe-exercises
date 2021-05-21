0ccc5ee6d269bb358d095cf490d3871c83dda762
Felipe Ventorim <felipeventorim11@gmail.com>
2021-05-21 17:31:08 -0300

    Exercicio 2 - imprimir triangulo retangulo de *

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/3.JavaScript-Lógica-de-Programação-e-Algoritmos/script2.js
@@ -0,0 +1,9 @@
+// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
+let n = 5;
+let linha = [];
+
+for (let indexLinha = 0; indexLinha < n; indexLinha++) {
+  linha += '*';
+  console.log(linha);
+}
