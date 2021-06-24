141839dce28a685ef9b8c01c73dec496a14820da
Felipe Ventorim <felipeventorim11@gmail.com>
2021-06-24 18:36:17 -0300

    exercicio 6 - some

--- bloco-8-Higher-Order-Functions-do-JavaScript-ES6/2.JavaScript-ES6--Higher-Order-Functions--forEach,-find,-some,-every,-sort/exercise6.js
+++ bloco-8-Higher-Order-Functions-do-JavaScript-ES6/2.JavaScript-ES6--Higher-Order-Functions--forEach,-find,-some,-every,-sort/exercise6.js
@@ -68,7 +68,7 @@ const books = [
 const expectedResult = true;
 
 function someBookWasReleaseOnThe80s() {
-  // escreva seu código aqui
+  return books.some((book) => book.releaseYear >= 1980 && book.releaseYear < 1990);
 }
 
 assert.strictEqual(someBookWasReleaseOnThe80s(), expectedResult);
