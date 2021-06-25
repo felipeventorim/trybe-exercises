d734e23b5dde0a6a5410138ebf412e475075bfe4
Felipe Ventorim <felipeventorim11@gmail.com>
2021-06-25 19:00:31 -0300

    Exercicio 5 - filter & map

--- bloco-8-Higher-Order-Functions-do-JavaScript-ES6/3.JavaScript -S6--Higher-Order-Functions--map-e-filter/exercise5.js
+++ bloco-8-Higher-Order-Functions-do-JavaScript-ES6/3.JavaScript -S6--Higher-Order-Functions--map-e-filter/exercise5.js
@@ -1,8 +1,6 @@
 const assert = require('assert');
 const books = require('./exercise1');
 
-// 5 - Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
-
 const expectedResult = [
   'Frank Herbert',
   'George R. R. Martin',
@@ -10,8 +8,13 @@ const expectedResult = [
   'J. R. R. Tolkien',
 ];
 
+// 5 - Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
+
 function fantasyOrScienceFictionAuthors() {
-  // escreva seu código aqui
+  const objectAuthors = books.filter((book) => 
+    book.genre === 'Ficção Científica' || book.genre === 'Fantasia');
+  const authors = objectAuthors.map((book) => book.author.name);
+  return authors.sort();
 }
 
 assert.deepStrictEqual(fantasyOrScienceFictionAuthors(), expectedResult);
