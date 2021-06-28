b77e26ad05ff1836f1861ee270708e5b795d3b88
Felipe Ventorim <felipeventorim11@gmail.com>
2021-06-28 18:01:29 -0300

    Exercicio 4 - Encontre o livro com o maior nome.

--- bloco-8-Higher-Order-Functions-do-JavaScript-ES6/4.JavaScript-ES6--Higher-Order-Functions--reduce/exercise4.js
+++ bloco-8-Higher-Order-Functions-do-JavaScript-ES6/4.JavaScript-ES6--Higher-Order-Functions--reduce/exercise4.js
@@ -6,7 +6,8 @@ const expectedResult = 'As Crônicas de Gelo e Fogo';
 // 4 - Encontre o livro com o maior nome.
 
 function longestNamedBook() {
-  // escreva seu código aqui
+  return books
+    .reduce((acc, book) => (book.name.length > acc.length ? book.name : acc), '');
 }
 
 assert.deepStrictEqual(longestNamedBook(), expectedResult);
