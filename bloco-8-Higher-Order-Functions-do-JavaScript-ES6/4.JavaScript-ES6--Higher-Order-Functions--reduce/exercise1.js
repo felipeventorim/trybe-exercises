3ea4621f6af448ce97de49dbb5def1ca4d755351
Felipe Ventorim <felipeventorim11@gmail.com>
2021-06-28 18:18:26 -0300

    Exercicio 1 - Dada uma matriz, transforme em um array.

--- bloco-8-Higher-Order-Functions-do-JavaScript-ES6/4.JavaScript-ES6--Higher-Order-Functions--reduce/exercise1.js
+++ bloco-8-Higher-Order-Functions-do-JavaScript-ES6/4.JavaScript-ES6--Higher-Order-Functions--reduce/exercise1.js
@@ -9,13 +9,7 @@ const arrays = [
 // 1 - Dada uma matriz, transforme em um array.
 
 function flatten() {
-  const transform = (acc, array) => {
-    array.forEach((element) => {
-      acc.push(element);
-    });
-    return acc;
-  };
-  return arrays.reduce(transform);
+  return arrays.reduce((acc, array) => acc.concat(array));
 }
 
 assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
