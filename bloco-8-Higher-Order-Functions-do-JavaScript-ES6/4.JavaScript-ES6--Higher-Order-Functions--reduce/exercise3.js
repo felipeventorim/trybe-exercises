1d1ccc69c593af21575452a0754c7bbc53dbe95a
Felipe Ventorim <felipeventorim11@gmail.com>
2021-06-28 15:26:36 -0300

    Exercicios bloco 8, dia 4

---
+++ bloco-8-Higher-Order-Functions-do-JavaScript-ES6/4.JavaScript-ES6--Higher-Order-Functions--reduce/exercise3.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const books = require('./exercise2');
+
+const expectedResult = 43;
+
+// 3 - Calcule a média de idade que as pessoas autoras tinham quando seus respectivos livros foram lançados.
+
+function averageAge() {
+  // escreva seu código aqui
+}
+
+assert.strictEqual(averageAge(), expectedResult);
---
+++ bloco-8-Higher-Order-Functions-do-JavaScript-ES6/4.JavaScript-ES6--Higher-Order-Functions--reduce/exercise4.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const books = require('./exercise2');
+
+const expectedResult = 'As Crônicas de Gelo e Fogo';
+
+// 4 - Encontre o livro com o maior nome.
+
+function longestNamedBook() {
+  // escreva seu código aqui
+}
+
+assert.deepStrictEqual(longestNamedBook(), expectedResult);
