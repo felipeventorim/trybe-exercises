07b341c4573d6761f05d9b5a105e538fa536a3e1
Felipe Ventorim <felipeventorim11@gmail.com>
2021-07-06 15:52:40 -0300

    Exercicio 6 - Por fim, o robô Curiosity tem uma taxa de sucesso de envio de mensagem de 60% devido ao fato de o robô já estar muito ocupado com outras operações. Logo, adicione na função sendMarsTemperature um outro callback que contenha as ações a serem tomadas em caso de falha.

--- bloco-9-JavaScript-Assíncrono-e-Promises/1.JavaScript-Assíncrono-e-Callbacks/exercise6.js
+++ bloco-9-JavaScript-Assíncrono-e-Promises/1.JavaScript-Assíncrono-e-Callbacks/exercise6.js
@@ -9,13 +9,23 @@ const getMarsTemperature = () => {
   return Math.floor(Math.random() * maxTemperature);
 };
 
-const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
-const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
-const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * (9 / 5)) + 32;
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+const greet = (temperature) =>
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
 const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`);
 
 // definição da função sendMarsTemperature...
+const sendMarsTemperature = (onSuccess, onError) => {
+  const marsTemperature = getMarsTemperature();
+  const MessageSentSuccessfully = Math.random() <= 0.6;
+  setTimeout(() => {
+    if (MessageSentSuccessfully) onSuccess(marsTemperature);
+    else onError('Robot is busy');
+  }, messageDelay());
+};
 
 // imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
 sendMarsTemperature(temperatureInFahrenheit, handleError);
