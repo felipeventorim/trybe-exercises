54eeb0008fd6afbc8caa518423e612369d8de45c
Felipe Ventorim <felipeventorim11@gmail.com>
2021-07-07 19:31:46 -0300

    Exercicio 2

---
+++ bloco-9-JavaScript-Assíncrono-e-Promises/2.JavaScript-Promises/exercise2/exercise2.js
@@ -0,0 +1,29 @@
+/*
+Agora, um passo para trás: vamos fazer, passo a passo, uma Promise
+Primeiramente, instancie uma Promise
+Dentro dela, você deve produzir um array com dez números aleatórios de 1 a 50 e elevá-los todos ao quadrado.
+Se a soma de todos esses elementos for inferior a 8000, a promise deve ser resolvida. Caso contrário, ela deve ser rejeitada. Acresça um then , com um console.log('Promise resolvida') e um catch , com um console.log('Promise rejeitada') à Promise , só para que o código funcione e possamos ver o resultado.
+Tente usar Higher Order Functions! Lembre-se de que tanto uma quanto a outra recebem, como parâmetro, funções!
+*/
+
+const randomNumbers = (max) => Math.ceil(Math.random() * max);
+
+const arrayRandomNumbers = (length, max) => {
+  const array = [];
+  for (let index = 0; index < length; index += 1) {
+    array.push(randomNumbers(max));
+  }
+  return array;
+};
+
+new Promise((resolve, reject) => {
+    const arrayRandom = arrayRandomNumbers(10, 50);
+    const summationNumbersSquared = arrayRandom.reduce((acc, number) => (acc + number ** 2));
+
+    if (summationNumbersSquared < 8000) {
+      return resolve(summationNumbersSquared);
+    }
+    reject();
+  })
+  .then(() => console.log('Promise resolvida'))
+  .catch(() => console.log('Promise rejeitada'));
