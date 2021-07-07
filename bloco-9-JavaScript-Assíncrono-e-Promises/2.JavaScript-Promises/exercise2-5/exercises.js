ff07ca7d61d2eb28e5ab3bbcc0638d2a7b8488bf
Felipe Ventorim <felipeventorim11@gmail.com>
2021-07-07 19:57:33 -0300

    Exercicio 2-5

--- bloco-9-JavaScript-Assíncrono-e-Promises/2.JavaScript-Promises/exercise2-5/exercises.js
+++ bloco-9-JavaScript-Assíncrono-e-Promises/2.JavaScript-Promises/exercise2-5/exercises.js
@@ -1,9 +1,15 @@
 /*
-Agora, um passo para trás: vamos fazer, passo a passo, uma Promise
+2. Agora, um passo para trás: vamos fazer, passo a passo, uma Promise
 Primeiramente, instancie uma Promise
 Dentro dela, você deve produzir um array com dez números aleatórios de 1 a 50 e elevá-los todos ao quadrado.
 Se a soma de todos esses elementos for inferior a 8000, a promise deve ser resolvida. Caso contrário, ela deve ser rejeitada. Acresça um then , com um console.log('Promise resolvida') e um catch , com um console.log('Promise rejeitada') à Promise , só para que o código funcione e possamos ver o resultado.
 Tente usar Higher Order Functions! Lembre-se de que tanto uma quanto a outra recebem, como parâmetro, funções!
+
+3. Quando a promise for resolvida com sucesso, retorne um array com 4 itens, sendo eles o resultado da divisão do numero resultante por 2, 3, 5 e 10.
+
+4. Quando a Promise for rejeitada, imprima, via console.log , a frase "É mais de oito mil! Essa promise deve estar quebrada!"
+
+5. Quando a Promise for bem-sucedida, encadeie nela uma segunda Promise que some os elementos do array.
 */
 
 const randomNumbers = (max) => Math.ceil(Math.random() * max);
@@ -16,14 +22,21 @@ const arrayRandomNumbers = (length, max) => {
   return array;
 };
 
-new Promise((resolve, reject) => {
+const fetchPromise = () => {
+  const promise = new Promise((resolve, reject) => {
     const arrayRandom = arrayRandomNumbers(10, 50);
     const summationNumbersSquared = arrayRandom.reduce((acc, number) => (acc + number ** 2));
 
     if (summationNumbersSquared < 8000) {
       return resolve(summationNumbersSquared);
     }
-    reject();
-  })
-  .then(() => console.log('Promise resolvida'))
-  .catch(() => console.log('Promise rejeitada'));
+    reject('É mais de oito mil! Essa promise deve estar quebrada!');
+  });
+
+  promise
+  .then((summation) => [2, 3, 5, 10].map((number) => summation / number))
+  .then((array) => console.log(array.reduce((acc, number) => acc + number)))
+  .catch((error) => console.log(error));
+};
+
+fetchPromise();
