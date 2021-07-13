bd591cf30c73a50d6f4704db1da8b782218c43eb
Felipe Ventorim <felipeventorim11@gmail.com>
2021-07-13 20:40:53 -0300

    Exercicio 2

--- bloco-10-Testes-automatizados-com-Jest/1.Primeiros-passos-no-Jest/exercise1/exercise2.test.js
+++ bloco-10-Testes-automatizados-com-Jest/1.Primeiros-passos-no-Jest/exercise1/exercise2.test.js
@@ -4,10 +4,8 @@
 // Verifique se o array passado por parâmetro não sofreu alterações
 // Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
 
-const assert = require('assert');
-
 function myRemove(arr, item) {
-  let newArr = [];
+  const newArr = [];
   for (let index = 0; index < arr.length; index += 1) {
     if (item !== arr[index]) {
       newArr.push(arr[index]);
@@ -17,3 +15,22 @@ function myRemove(arr, item) {
 }
 
 // implemente seus testes aqui
+describe('myRemove(arr, item)', () => {
+  it('should return the array [1, 2, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
+
+  it('should not return the array [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+
+  it('the array passed by parameter must not change', () => {
+    const array = [1, 2, 3, 4];
+    myRemove(array, 3);
+    expect(array).toEqual([1, 2, 3, 4]);
+  });
+
+  it('should return the array [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+});
