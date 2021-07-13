d770d499c61c71b674d31d202a255f55f441d765
Felipe Ventorim <felipeventorim11@gmail.com>
2021-07-13 20:43:08 -0300

    Exercicio 3

--- bloco-10-Testes-automatizados-com-Jest/1.Primeiros-passos-no-Jest/exercise1/exercise3.test.js
+++ bloco-10-Testes-automatizados-com-Jest/1.Primeiros-passos-no-Jest/exercise1/exercise3.test.js
@@ -4,8 +4,6 @@
 // Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações
 // Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado
 
-const assert = require('assert');
-
 function myRemoveWithoutCopy(arr, item) {
   for (let index = 0, len = arr.length; index < len; index += 1) {
     if (arr[index] === item) {
@@ -19,3 +17,22 @@ function myRemoveWithoutCopy(arr, item) {
 }
 
 // implemente seus testes aqui
+describe('myRemoveWithoutCopy(arr, item)', () => {
+  it('should return the array [1, 2, 4]', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
+
+  it('should not return the array [1, 2, 3, 4]', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+
+  it('the array passed by parameter must change', () => {
+    const array = [1, 2, 3, 4];
+    myRemoveWithoutCopy(array, 3);
+    expect(array).toEqual([1, 2, 4]);
+  });
+
+  it('should return the array [1, 2, 3, 4]', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+});
