0bbc6e19391c26007d40fdd238ce0ffcb895bab5
Felipe Ventorim <felipeventorim11@gmail.com>
2021-07-14 20:21:44 -0300

    Exercicio 3

--- bloco-10-Testes-automatizados-com-Jest/2.Jest-Testes-Assincronos/exercises.js/exercise3.test.js
+++ bloco-10-Testes-automatizados-com-Jest/2.Jest-Testes-Assincronos/exercises.js/exercise3.test.js
@@ -1,1 +1,18 @@
 // 3 - Reescreva o teste do exercício anterior, desta vez utilizando a sintaxe de async/await .
+
+const getUserName = require('./exercise2-3');
+
+describe('test getUserName with async/await', () => {
+  it('should have find the user Mark', async () => {
+    const result = await getUserName(4);
+    expect(result).toBe('Mark');
+  });
+
+  it('should not have find the id 3', async () => {
+    try {
+      await getUserName(3);
+    } catch (error) {
+      expect(error).toEqual({ error: 'User with 3 not found.' });
+    }
+  });
+});
