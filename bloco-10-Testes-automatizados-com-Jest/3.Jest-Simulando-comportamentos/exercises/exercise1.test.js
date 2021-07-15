6b5895624ad8d77fd141290989c1de72ff1ecefc
Felipe Ventorim <felipeventorim11@gmail.com>
2021-07-15 18:47:31 -0300

    Exercicio 1 - Crie uma função que gere um número aleatório entre 0 e 100. Você irá criar também os testes para essa função. Utilizando o mock, defina o retorno padrão como 10. Teste se a função foi chamada, qual seu retorno e quantas vezes foi chamada.

--- bloco-10-Testes-automatizados-com-Jest/3.Jest-Simulando-comportamentos/exercises/exercise1.test.js
+++ bloco-10-Testes-automatizados-com-Jest/3.Jest-Simulando-comportamentos/exercises/exercise1.test.js
@@ -1,2 +1,15 @@
 // 1. Crie uma função que gere um número aleatório entre 0 e 100. Você irá criar também os testes para essa função.
 // Utilizando o mock, defina o retorno padrão como 10. Teste se a função foi chamada, qual seu retorno e quantas vezes foi chamada.
+
+let randomNumber = () => Math.floor(Math.random() * 100);
+
+test('should have return 10', () => {
+  randomNumber = jest
+    .fn()
+    .mockReturnValue(10);
+  
+  randomNumber();
+  expect(randomNumber).toHaveBeenCalled();
+  expect(randomNumber()).toBe(10);
+  expect(randomNumber).toHaveBeenCalledTimes(2);
+});
