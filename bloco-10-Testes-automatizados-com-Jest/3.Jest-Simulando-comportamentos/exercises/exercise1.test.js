ef6b41f937ed2f396ac0166821f979900b20fabb
Felipe Ventorim <felipeventorim11@gmail.com>
2021-07-15 19:01:02 -0300

    Exercicio 2 - Com a mesma função do exercício anterior, utilizando o mock, crie uma nova implementação, que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo. Essa implementação deve ocorrer uma única vez. Faça os testes necessários.

--- bloco-10-Testes-automatizados-com-Jest/3.Jest-Simulando-comportamentos/exercises/exercise1.test.js
+++ bloco-10-Testes-automatizados-com-Jest/3.Jest-Simulando-comportamentos/exercises/exercise1.test.js
@@ -3,7 +3,7 @@
 
 let randomNumber = () => Math.floor(Math.random() * 100);
 
-test('should have return 10', () => {
+test('should call the function, what is its return and how many times it was called', () => {
   randomNumber = jest
     .fn()
     .mockReturnValue(10);
@@ -13,3 +13,5 @@ test('should have return 10', () => {
   expect(randomNumber()).toBe(10);
   expect(randomNumber).toHaveBeenCalledTimes(2);
 });
+
+module.exports = randomNumber;
--- bloco-10-Testes-automatizados-com-Jest/3.Jest-Simulando-comportamentos/exercises/exercise2.test.js
+++ bloco-10-Testes-automatizados-com-Jest/3.Jest-Simulando-comportamentos/exercises/exercise2.test.js
@@ -1,3 +1,15 @@
 // 2. Com a mesma função do exercício anterior, utilizando o mock, crie uma nova implementação,
 // que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo. Essa
 // implementação deve ocorrer uma única vez. Faça os testes necessários.
+
+const randomNumber = require('./exercise1.test');
+
+jest.mock('./exercise1.test');
+
+test('should call the function, what is its return and how many times it was called', () => {
+  randomNumber.mockImplementationOnce((a, b) => a / b);
+
+  expect(randomNumber(4, 2)).toBe(2);
+  expect(randomNumber).toHaveBeenCalledTimes(1);
+  expect(randomNumber).toHaveBeenCalledWith(4, 2);
+});
