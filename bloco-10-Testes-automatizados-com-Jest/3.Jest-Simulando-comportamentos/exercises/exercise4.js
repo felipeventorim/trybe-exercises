774d2086ff37e9a955a5795f005b85309087deb0
Felipe Ventorim <felipeventorim11@gmail.com>
2021-07-15 19:32:43 -0300

    Exercicio 4 - Dentro de um mesmo arquivo, crie três funções. A primeira deve receber uma string e retorná-la em caixa alta. A segunda deve também receber uma string e retornar só a primeira letra. A terceira deve receber duas strings e concatená-las. Faça o mock do arquivo. Faça uma nova implementação para a primeira função, mas agora ela deve retornar a string em caixa baixa. Para a segunda função, uma nova implementação deve retornar a última letra de uma string. A terceira deve receber três strings e concatená-las.

---
+++ bloco-10-Testes-automatizados-com-Jest/3.Jest-Simulando-comportamentos/exercises/exercise4.js
@@ -0,0 +1,7 @@
+const upperCase = (string) => string.toUpperCase();
+
+const firstLetter = (string) => string[0];
+
+const stringsConcat = (str1, str2) => `${str1}${str2}`;
+
+module.exports = { upperCase, firstLetter, stringsConcat };
